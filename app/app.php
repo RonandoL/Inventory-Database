314077bcbabfee6a46159abbd7cb04d4b54c67f8
Mary Warrington and Ronando <ronando.long@gmail.com>
2016-02-22 16:40:49 -0800

    Add twig functionality

--- app/app.php
+++ app/app.php
@@ -4,6 +4,11 @@
 
     $app = new Silex\Application();
 
+    $server = 'mysql:host=localhost;dbname=inventory_database';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views'
     ));
@@ -14,6 +19,27 @@
     $app->get("/", function() use ($app) {
         return $app['twig']->render('inventory.html.twig'); //
     });
+
+    // Show all items
+    $app->get("/items", function() use ($app) {
+        return $app['twig']->render('inventory.html.twig', array('items' => Inventory::getAll()));
+    });
+
+    // Post an item(s) added to inventory
+    $app->post("/items", function() use ($app) {
+        $item = new Inventory($_POST['name']);
+        $item->save();
+        return $app['twig']->render('inventory.html.twig', array('items' => Inventory::getAll()));
+    });
+
+    // Delete all items from table
+    $app->post("/delete_items", function() use ($app) {
+        Inventory::deleteAll();
+        return $app['twig']->render('inventory.html.twig');
+    });
+
+
+
 
     return $app;
 
